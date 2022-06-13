6b31807001015553e87a2ab2de81f05ecf63aeef
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-13 23:56:36 +0300

    Add api url

--- app/urls.py
+++ app/urls.py
@@ -1,1 +1,8 @@
 from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('api/projects/', views.Project.as_views()),
+
+]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .models import  MoringaMerch
+from .serializer import ProjectSerializer
 
 # Create your views here.
-def Home(request):
-    return render(request)
+def home(request):
+    return render(request, 'home.html',context)
+
+
+class Project(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
