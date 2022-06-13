4736405ce71f49f47295f2eeeeecb37290f829cf
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-13 23:27:02 +0300

    Add project model

--- app/models.py
+++ app/models.py
@@ -2,9 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Project(models.Models):
+    image=models.ImageField(upload_to='images/', null=True, blank=True)
+    title=models.CharField(max_length=30)
+    description=models.TextField(max_length=100)
+    url= models.URLField(max_length=200)
+
+
 class Profile(models.Models):
     user = models.OneToOneField(User,primary_key=True, on_delete=models.CASCADE)
     image = models.ImageFiel(upload_to='profile_pics/', null=True)
     bio = models.TextField()
+
+
+
+
 
 
