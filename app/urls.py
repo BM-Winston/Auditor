02b28e548c6b8ed55d0e239b82b4307c33f0f64a
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-14 23:00:42 +0300

    Fix recurssion error

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from . import views
 from django.urls import path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('api/projects/', views.Project.as_views()),
+    path('api/projects/', views.ProjectView.as_view()),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,24 +1,6 @@
 from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status
-from .models import  MoringaMerch
-from .serializer import ProjectSerializer
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',context)
+    return render(request, 'home.html')
 
-
-class Project(APIView):
-    def get(self, request, format=None):
-        all_projects = Project.objects.all()
-        serializers = ProjectSerializer(all_projects, many=True)
-        return Response(serializers.data)
-
-    def post(self, request, format=None):
-        serializers = ProjectSerializer(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
