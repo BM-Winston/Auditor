85e1d5691c2fee09096cd4958965cbdcf7426219
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-18 01:04:50 +0300

    Deployment

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/auditor/', views.AuditorView.as_view()),
-    path('home/',views.home,name = 'home'),
+    path('',views.home,name = 'home'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
